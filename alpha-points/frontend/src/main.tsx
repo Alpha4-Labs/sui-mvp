99f1e015433db0fc8ebb4986e147524793367809
TRC-Crypto <taylorcoxdesigns@gmail.com>
2025-05-05 00:19:01 -0400

    MVP Ready

--- alpha-points/frontend/src/main.tsx
+++ alpha-points/frontend/src/main.tsx
@@ -1,7 +1,10 @@
+console.log("--- main.jsx starting execution ---");
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+
+console.log("--- Rendering React root ---"); // <--- Maybe add this too
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
